c628521b34c34151b74db3b0521f5b17e3f10549
Jon Goldson <forjrg@gmail.com>
2020-11-25 14:45:40 -0700

    Adding some initial velocity to the crate pieces when a crate breaks apart

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -6,6 +6,8 @@ public class Box : MonoBehaviour
 {
     Rigidbody2D myRigidBody;
     [SerializeField] GameObject[] cratePieces;
+    [SerializeField] float max_Breaking_Velocity = 5f;
+    [SerializeField] float min_Breaking_Velocity = 1f;
     
 
     private void Start() {
@@ -25,6 +27,9 @@ public class Box : MonoBehaviour
                 foreach (GameObject cratePiece in cratePieces) {
                     GameObject newPiece = 
                         Instantiate(cratePiece, transform.position, transform.rotation) as GameObject;
+                    newPiece.GetComponent<Rigidbody2D>().velocity = 
+                    new Vector2(Random.Range(-max_Breaking_Velocity, max_Breaking_Velocity),
+                        Random.Range(min_Breaking_Velocity, max_Breaking_Velocity));
                     Destroy(newPiece, 1f);
                 }
                 Destroy(gameObject);
