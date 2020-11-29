a27baee65767ca0f5deac33fa4a3860dc2a61995
Jon Goldson <forjrg@gmail.com>
2020-11-29 11:57:28 -0700

    Adding in cannon sounds

--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -6,12 +6,14 @@ public class CannonBall : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
     [SerializeField] GameObject explosionPrefab;
+    [SerializeField] AudioClip boomSound;
     
 
     void Update() {
         transform.Translate(Vector2.left * speed * Time.deltaTime);
     }
     private void OnTriggerEnter2D(Collider2D other) {
+        AudioSource.PlayClipAtPoint(boomSound, transform.position);
         GameObject explosionParticles = Instantiate(
             explosionPrefab,
             transform.position,
