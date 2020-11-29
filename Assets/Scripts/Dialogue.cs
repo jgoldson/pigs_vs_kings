6404b89400ee30a7cb813506710d0bba33dde8f8
Jon Goldson <forjrg@gmail.com>
2020-11-29 13:17:32 -0700

    Adding basic dialogue box to first level

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Dialogue : MonoBehaviour
+{
+    Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        player.StopPlayerMovement();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1")) {
+            player.AllowPlayerMovement();
+            gameObject.SetActive(false);
+            Destroy(gameObject, 1);
+        }
+    }
+}
