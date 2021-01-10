5cce230f380288c6f8a26c10018c2b588dd522b7
Jon Goldson <forjrg@gmail.com>
2021-01-09 17:31:10 -0700

    changed queen piggy to be slightly larger and added blue bird sprite which has a toggle to follow queen piggy around

---
+++ Assets/Game/Scripts/Bird.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField] bool FollowPlayer = true;
+
+    Player player;
+    float xDistanceToFollow = 0.8f;
+    float yDistanceToFollow = 0.3f;
+    float timeToChangeDistance = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToChangeDistance -= Time.deltaTime;
+        if (timeToChangeDistance < 0f) {
+            xDistanceToFollow = Random.Range(0.7f, 0.9f);
+            yDistanceToFollow = Random.Range(0.2f, 0.4f);
+            timeToChangeDistance = Random.Range(3f, 8f);
+        }
+        if (FollowPlayer) {
+        Vector2 playerPos = player.transform.position;
+        float playerDirection = player.transform.localScale.x;
+        gameObject.transform.position = new Vector2(playerPos.x + (-xDistanceToFollow * playerDirection), playerPos.y + yDistanceToFollow);
+        gameObject.transform.localScale = new Vector2 (-playerDirection, 1f); 
+        }
+    }
+}
