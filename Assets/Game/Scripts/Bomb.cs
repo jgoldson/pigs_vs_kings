f406f7fa83d1317fa78457c0f3bf46c61146afb8
Jon Goldson <forjrg@gmail.com>
2021-01-11 10:22:43 -0700

    Added in pig who can throw a bomb, the bomb isnt hazardous but will leave a large explosion that will kill the player

---
+++ Assets/Game/Scripts/Bomb.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] float timeUntilExplode = 3;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeUntilExplode -= Time.deltaTime;
+        if (timeUntilExplode <= 0){
+            animator.SetTrigger("BombExplode");
+            
+        }
+    }
+    public void DestroyBomb(){
+        Destroy(gameObject);
+    }
+    public void Explosion(){
+        this.gameObject.transform.GetChild(0).GetComponent<CircleCollider2D>().enabled = true;
+    }
+}
